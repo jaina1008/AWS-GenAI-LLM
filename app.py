74b210eb189f3b68913a855d17a7ed4c92f3c307
jaina1008 <mailme.anshuljain@gmail.com>
2024-10-18 09:54:40 +1300

    app.py updated

--- app.py
+++ app.py
@@ -1,65 +1,81 @@
 import boto3
 import botocore.config
 import json
-
 from datetime import datetime
 
-def blog_generate_using_bedrock(blogtopic:str)-> str:
-    prompt=f"""<s>[INST]Human: Write a 200 words blog on the topic {blogtopic}
+def blog_generate_using_bedrock(blogTopic: str) -> str:
+    prompt=f"""<s>[INST]Human: Write a 200 word blog on the topic {blogTopic}
     Assistant:[/INST]
     """
 
-    body={
-        "prompt":prompt,
-        "max_gen_len":512,
-        "temperature":0.5,
-        "top_p":0.9
+    body = {
+        "inputText": prompt,
+        "textGenerationConfig": {
+            "maxTokenCount": 4096,
+            "stopSequences": [],
+            "temperature": 0.5,
+            "topP": 0.9
+        }
     }
 
+    bedrock=boto3.client("bedrock-runtime", region_name="ap-southeast-2",
+                             config=botocore.config.Config(read_timeout=300, retries={"max_attempts":3}))
+    
     try:
-        bedrock=boto3.client("amazon.titan-text-lite-v1",region_name="ap-southeast-2",
-                             config=botocore.config.Config(read_timeout=300,retries={'max_attempts':3}))
-        response=bedrock.invoke_model(body=json.dumps(body),modelId="meta.llama2-13b-chat-v1")
+        # Ensure you set the modelId correctly
+        response = bedrock.invoke_model(
+            modelId="amazon.titan-text-lite-v1",
+            contentType="application/json",
+            accept="application/json",
+            body=json.dumps(body)  # Ensure the body is JSON encoded
+        )
 
-        response_content=response.get('body').read()
+        response_content=response['body'].read()
         response_data=json.loads(response_content)
         print(response_data)
+        
         blog_details=response_data['generation']
         return blog_details
+        
     except Exception as e:
-        print(f"Error generating the blog:{e}")
+        print(f"Error generating the blog: {e}")
         return ""
 
-def save_blog_details_s3(s3_key,s3_bucket,generate_blog):
-    s3=boto3.client('s3')
 
+def save_blog_details_s3(s3_key, s3_bucket, generate_blog):
+    s3=boto3.client('s3')
     try:
-        s3.put_object(Bucket = s3_bucket, Key = s3_key, Body =generate_blog )
+        s3.put_object(Bucket=s3_bucket, Key=s3_key, Body=generate_blog)
         print("Code saved to s3")
 
-    except Exception as e:
-        print("Error when saving the code to s3")
+    except:
+        pass
 
 
 
-def lambda_handler(event, context):
+def lambda_handler(event, content):
 
     event=json.loads(event['body'])
-    blogtopic=event['blog_topic']
+    blogTopic=event['blog_topic']
 
-    generate_blog=blog_generate_using_bedrock(blogtopic=blogtopic)
+    generate_blog=blog_generate_using_bedrock(blogTopic=blogTopic)
+    body = ""
 
     if generate_blog:
+        body = "Blog Generation Completed."
+
         current_time=datetime.now().strftime('%H%M%S')
         s3_key=f"blog-output/{current_time}.txt"
-        s3_bucket='aws_bedrock_course1'
-        save_blog_details_s3(s3_key,s3_bucket,generate_blog)
+        s3_bucket='bedrockawsexample'
 
+        save_blog_details_s3(s3_key, s3_bucket, generate_blog)
+        body += "Saved to S3."
 
     else:
-        print("No blog was generated")
-
+        body = "No blog was generated."
+        print(f"No blog was generated.")
+    
     return{
-        'statusCode':200,
-        'body':json.dumps('Blog Generation is completed')
+        "statusCode": 200,
+        "body": json.dumps(body)
     }
